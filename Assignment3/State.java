68d6e3d801a2b8a57140ec254ea6bdeba6c653ae
meta-animesh-agarwal <35561483+meta-animesh-agarwal@users.noreply.github.com>
2018-02-17 10:54:21 +0530

    This is my 3 and 4 assignment.Sorry for delay.

---
+++ Assignment3/State.java
@@ -0,0 +1,21 @@
+package session3;
+
+/**
+ * 
+ * This class represents the various state of workflow
+ * @author Animesh Agarwal
+ */
+public class State {
+	
+	//variable storing name of the workflow
+	String stateName;
+	
+	/**
+	 * constructor initializing statename
+	 * @param {String}stateName name of the state
+	 */
+	State(String stateName){
+		this.stateName = stateName;
+	}
+	
+}
